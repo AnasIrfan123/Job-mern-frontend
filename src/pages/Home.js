6ea94a68fa5fb582b6a92dfd26c76c4469da1a6c
AnasIrfan123 <ai8619233@gmail.com>
2024-06-26 12:26:35 +0500

    work incomplete

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from '../component/Navbar'
+import Header from '../component/Header'
+
+export default function Home() {
+  return (
+    <>
+        <Navbar />
+        <Header />
+
+        <h1>Home Page</h1>
+    </>
+  )
+}
